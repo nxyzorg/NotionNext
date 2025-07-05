8827f6d32424ab9c96947959fbc84f0a630aeadd
Nxyz <ovwlive@gmail.com>
2025-07-05 17:05:33 +0800

    Update PoweredBy.js

--- components/PoweredBy.js
+++ components/PoweredBy.js
@@ -6,7 +6,7 @@ import { siteConfig } from '@/lib/config'
  */
 export default function PoweredBy(props) {
   return (
-    <div className={`inline text-sm font-serif ${props.className || '123'}`}>
+    <div className={`inline text-sm font-serif ${props.className || ''}`}>
       <span className='mr-1'>Powered by</span>
       <a
         href='https://github.com/nxyzorg/NotionNext'
