13a4f58c429c79a8f3cd12179ebb7b6838ad5a1c
Nxyz <ovwlive@gmail.com>
2025-07-05 16:42:15 +0800

    Update contact.config.js

--- conf/contact.config.js
+++ conf/contact.config.js
@@ -1,7 +1,7 @@
 /**
  * 社交按钮相关的配置同意放这
  */
-module。exports = {
+module.exports = {
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
   CONTACT_WEIBO: process.env.NEXT_PUBLIC_CONTACT_WEIBO || '', // 你的微博个人主页
