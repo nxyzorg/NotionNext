8f6492c9989a51e87d75238e8f60043e136e105a
Nxyz <ovwlive@gmail.com>
2025-07-05 17:24:58 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -39,7 +39,6 @@ const CONFIG = {
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   HEO_INFOCARD_GREETINGS: [
-    '你好！我是',
     '🔍 分享与热心帮助',
     '🤝 专修交互与设计',
     '🏃 脚踏实地行动派',
@@ -51,9 +50,9 @@ const CONFIG = {
   // 个人资料底部按钮
   HEO_INFO_CARD_URL1: '/about',
   HEO_INFO_CARD_ICON1: 'fas fa-user',
-  HEO_INFO_CARD_URL2: 'https://github.com/tangly1024',
+  HEO_INFO_CARD_URL2: 'https://github.com/nxyzorg',
   HEO_INFO_CARD_ICON2: 'fab fa-github',
-  HEO_INFO_CARD_URL3: 'https://www.tangly1024.com',
+  HEO_INFO_CARD_URL3: 'https://licheng.me',
   HEO_INFO_CARD_TEXT3: '了解更多',
 
   // 用户技能图标
